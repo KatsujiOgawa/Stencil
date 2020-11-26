fbb0fb38901ba8ce76a782ab025c575020bd0170
KatsujiOgawa <marug711@gmail.com>
2020-11-26 19:21:42 +0900

    add: ユーザー詳細ページのルーティングとコントローラー生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   get 'reviews/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show]
   root to: "reviews#top"
   resources :reviews
 end
