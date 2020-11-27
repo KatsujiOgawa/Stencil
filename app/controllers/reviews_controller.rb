3e3baf23cf622bfb46ed4ff0b093003993703457
KatsujiOgawa <marug711@gmail.com>
2020-11-27 18:01:39 +0900

    add: reviewsテーブルに保存するストロングパラメーターの定義

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,6 +6,16 @@ class ReviewsController < ApplicationController
   end
 
   def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to reviews_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -15,4 +25,8 @@ class ReviewsController < ApplicationController
   end
   
 
+  private
+  def review_params
+    params.require(:review).permit(:title, :image, :text, :category_id, :limit_id).merge(user_id: current_user.id)
+  end
 end
