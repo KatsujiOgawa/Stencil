edf5cc23e7f91afa5a3102468cedd2f4e037767e
KatsujiOgawa <marug711@gmail.com>
2020-11-29 21:54:40 +0900

    add: commentカラムにバリデーション を設定

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :review, foreign_key: :review_id, dependent: :destroy
+
+  validates :comment, presence: true, length: {maximum: 50 } 
 end
