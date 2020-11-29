033edd872c76bf1f826774e13641cf4ab6235899
KatsujiOgawa <marug711@gmail.com>
2020-11-30 01:27:14 +0900

    add: reviewモデル単体テストコードの実装

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :review do
-    
+    title        {"７つの習慣"}
+    text         {Faker::Lorem.paragraph(sentence_count = 3)}
+    category_id  {"10"}
+    limit_id     {"3"}
+    association :user 
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/index_contents.jpg'), filename: 'index_contents.jpg')
+    end
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    name                  {Faker::Name.name}
+    name                  {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
     password              {"abc123"}
     password_confirmation {password}
