7d4ef367acf8c4e50e4e5571d66100534f505413
KatsujiOgawa <marug711@gmail.com>
2020-12-01 19:27:08 +0900

    add: ログインユーザーの最新の投稿日時を取得

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+  before_action :get_hurry
+
+  def get_hurry
+    if user_signed_in?
+      @last_reviews = Review.where(user_id: current_user.id).order(created_at: :desc).limit(1)
+    end
+  end
+
 private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
