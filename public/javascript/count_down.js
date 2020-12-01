d5dbe2bc5d7cc9b65b008197ca29b1d2b2cd4566
KatsujiOgawa <marug711@gmail.com>
2020-12-01 17:54:10 +0900

    add: カウントダウンの機能機能を実装

--- public/javascript/count_down.js
+++ public/javascript/count_down.js
@@ -1,3 +1,19 @@
 window.addEventListener('load', function(){
-  console.log('読み込み確認')
+  var sec = 86400;
+  let dt = new Date();
+  let endDt = new Date(dt.getTime() + sec * 1000);
+  console.log( dt)
+  console.log(endDt)
+
+  let cnt = sec;
+  let id = setInterval(function(){
+    cnt--;
+    console.log(cnt);
+    // 現在日時と終了日時を比較
+    dt = new Date();
+    if(dt.getTime() >= endDt.getTime()){
+      clearInterval(id);
+      console.log("Finish!");
+    }
+  }, 1000);
 })
