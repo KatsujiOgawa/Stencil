f917d43d02a75a7858b19469e3fa7170c60c33e2
KatsujiOgawa <marug711@gmail.com>
2020-12-17 14:11:52 +0900

    update:count_down.jsのリファクタリング

--- public/javascript/count_down.js
+++ public/javascript/count_down.js
@@ -1,45 +1,45 @@
 
 window.addEventListener('load', function(){
-  const latestReview = document.getElementById("limit_date")
-  const limitId = latestReview.getAttribute("data-limit_id")
+  // 最新投稿の定義
+  const lastReview = document.getElementById("limit_date")
+  // 設定したタイムリミットを取得し、エポックミリ秒換算
+  const limitId = lastReview.getAttribute("data-limit_id")
   const oneDay = 24 * 60 * 60 * 1000;
   const days = oneDay * limitId;
-  let createDt = latestReview.getAttribute("data-created_at");
-  console.log(createDt);
+  // 最新の投稿日時を定義
+  let createDt = lastReview.getAttribute("data-created_at");
   let startDt = new Date(createDt);
   let startSec = startDt.getTime()
   
+  // 投稿期限日の定義
+  let endDt = new Date(startSec + days);
+  let endSec = endDt.getTime();  
+
+  // カウントダウン処理
   let id = setInterval(function(){
+    // 1秒毎に現在時刻と期限日の差を計算
     let dt = new Date();
-    let endDt = new Date(startSec + days);
-    let endSec = endDt.getTime();  
     let untilLimit = endSec - dt;
-    console.log(startDt)
-    console.log(endDt)
-    untilLimit--;    
+    untilLimit--;
+    // 年月日時分秒に換算
     d = Math.floor(untilLimit / oneDay);
     h = Math.floor((untilLimit % oneDay) / (60 * 60 * 1000));
     m = Math.floor((untilLimit % oneDay) / (60 * 1000)) % 60;
     s = Math.floor((untilLimit % oneDay) / 1000) % 60 % 60;
-    // console.log(untilLimit);
     console.log(`${d}日${h}時間${m}分${s}秒`);
     
-    // 現在日時と終了日時を比較
+    // タイムリミットになったらカウントダウンを終了する条件分岐
     dt = new Date();
     if(dt.getTime() < endDt){
       d = Math.floor(untilLimit / oneDay);
       h = Math.floor((untilLimit % oneDay) / (60 * 60 * 1000));
       m = Math.floor((untilLimit % oneDay) / (60 * 1000)) % 60;
       s = Math.floor((untilLimit % oneDay) / 1000) % 60 % 60;
-      latestReview.innerHTML = `${d}日${h}時間${m}分${s}秒`
+      lastReview.innerHTML = `${d}日${h}時間${m}分${s}秒`
     } else {
       clearInterval(id);
       console.log("Finish!");
-      latestReview.innerHTML = `レビュー終了`
+      lastReview.innerHTML = `レビュー終了`
     }
   }, 1000);
 })
-
-// ・修正点予定ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
-// ページの読み込みから1秒カウントダウンの表示までラグがある。
-// 最新投稿のcreated_atが秒単位で取得できていない
